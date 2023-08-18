215421f66436b5d288d854ca0da37e1bfeea712e
scupak <antonibujko@hotmail.com>
2023-08-18 15:52:59 +0200

    stuff

---
+++ TOH/src/app/heroes/heroes.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import {HeroService} from "../services/hero.service";
+import {Observable} from "rxjs";
+import {MessageService} from "../services/message.service";
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  heroes: Hero[] = [];
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+
+
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+}
